911cba6207b439516577b3946ccca402afa1f2dc
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-02-18 13:42:07 +0700

    demo thêm sinh viên

--- asm/pages/HomeController.js
+++ asm/pages/HomeController.js
@@ -18,4 +18,15 @@ window.HomeController = function($scope, $http){
           $scope.subjects = response.data;
         }
       })
+
+    // $http.post('http://localhost:3000/students', {
+    //   username: "tuanba",
+    //   password: "iloveyou",
+    //   fullname: "Bùi Anh Tuấn",
+    //   email: "tuanba@fpt.edu.vn",
+    //   gender: "true",
+    //   birthday: "1995-12-21",
+    //   schoolfee: "1500000",
+    //   marks: "0"
+    // })
 }
